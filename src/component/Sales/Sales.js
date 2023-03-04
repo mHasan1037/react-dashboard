5ef45bd4d66d2bf67cde1c3bf9ec9c21d40d1938
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-03-04 23:10:08 +0600

    updating the homepage with some chart, sections

--- src/component/Sales/Sales.js
+++ src/component/Sales/Sales.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Doughnut } from 'react-chartjs-2';
 import style from './sales.module.css'
-import { IoIosArrowForward } from 'react-icons/io'
+import { IoIosArrowForward, IoIosArrowDown } from 'react-icons/io'
+import { AiOutlineWarning, AiOutlineArrowRight, AiOutlineArrowUp } from 'react-icons/ai'
 import Total from './Total'
+import trail from '../../images/trail.png'
 
 const totalData = [
     {
@@ -30,7 +33,55 @@ const totalData = [
     }
 ]
 
+
+const doughnutData = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(153, 102, 255, 0.6)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  
+  const options = {
+    responsive: true,
+    maintainAspectRatio: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+
+
 const Sales = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const [isOpenRight, setIsOpenRight] = useState(false)
+
+    const handleButtonClick = () =>{
+        setIsOpen(!isOpen)
+    }
+
+    const handleButtonClickRight = () =>{
+        setIsOpenRight(!isOpenRight)
+    }
+
   return (
     <div className={style.salesContainer}>
         <div className={style.salesHeadline}>
@@ -40,9 +91,105 @@ const Sales = () => {
             </div>
         </div>
         
-        <div className={style.totalContainer}>
+        <section className={style.totalContainer}>
              <Total totalData={totalData} />
-        </div>
+        </section>
+
+        {
+            /*
+            Sales section
+            */
+        }
+        <section className={style.salesSection}>
+             <div className={style.salesLeft}>
+                <div style={{backgroundColor: 'white', padding: '20px'}}>
+                    <div className={style.salesSt}>
+                        <div>
+                        <AiOutlineWarning style={{marginRight: '5px', color: '#947a32'}}/> 
+                        <span style={{color: '#937931'}}>Your free trial expired in <b>21</b> days.</span>
+                        </div>
+                        <a href='#' style={{color: '#937931', fontWeight: 'bold'}}>Upgrade</a>
+                    </div>
+                    <div className={style.salesNd}>
+                        <div className={style.salesUpgrade}>
+                            <div className={style.upgradeDes}>
+                                Upgrade your plan from a <span style={{fontWeight: 'bold', lineHeight: '35px'}}>Free trial,</span> to 'Premium Plan' 
+                                <AiOutlineArrowRight style={{marginLeft: '8px', fontSize: '14px'}}/>
+                            </div>
+                            <button>Upgrade Account!</button>
+                        </div>
+                        <div className={style.salesImgBox}>
+                              <img className={style.salesImg} src={trail} />
+                        </div>
+                    </div>
+                </div>
+                <div className={style.secondLeft}>
+                    <div className={style.reportHeadline}>
+                        <p style={{color: '#474e55'}}>Earning Reports</p>
+                        <div className={style.btnBox}>
+                                <button 
+                                    onClick={handleButtonClick} 
+                                    className={style.mainBtn}
+                                >
+                                    <span style={{fontWeight: 'bold'}}>Report By:</span> Monthly
+                                    <IoIosArrowDown style={{fontSize: '10px'}}/>
+                                </button>
+                                {isOpen && (
+                                <div className={style.btnOptions}>
+                                    <button>Monthly</button>
+                                    <button>Weekly</button>
+                                    <button>Yearly</button>
+                                </div>
+                                )}
+                        </div>
+                    </div>
+                    <div className={style.reportBody}>
+                        <div className={style.reportLeft}>
+                            <div className={style.reportMonths}>
+                                <div>
+                                    <p className={style.reportMonth}>This Month</p>
+                                    <h3 className={style.reportMoney}>$12, 582</h3>
+                                    <span className={style.reportPer}>+15%</span>
+                                </div>
+                                <div>
+                                    <p className={style.reportMonth}>Last Month</p>
+                                    <h3 className={style.reportMoney}>$98, 741</h3>
+                                </div>
+                            </div>
+                            <p className={style.reportPrev}><span style={{color: '#51d28c'}}>25.2% <AiOutlineArrowUp /></span> From previous period</p>
+                            <button>Generate Reports <AiOutlineArrowRight /></button>
+                        </div>
+                        <div className={style.reportRight}>
+                            <Doughnut style={{width: '100%'}} data={doughnutData} options={options} />
+                        </div>
+                    </div>
+                </div>
+             </div>
+
+             <div className={style.salesRight}>
+                    <div className={style.reportHeadline}>
+                        <p style={{color: '#474e55'}}>Earning Reports</p>
+                        <div className={style.btnBox}>
+                                <button 
+                                    onClick={handleButtonClickRight} 
+                                    className={style.mainBtn}
+                                >
+                                    <span style={{fontWeight: 'bold'}}>Report By:</span> Monthly
+                                    <IoIosArrowDown style={{fontSize: '10px'}}/>
+                                </button>
+                                {isOpenRight && (
+                                <div className={style.btnOptions}>
+                                    <button>Monthly</button>
+                                    <button>Weekly</button>
+                                    <button>Yearly</button>
+                                </div>
+                                )}
+                        </div>
+                    </div>
+
+                    
+             </div>
+        </section>
     </div>
   )
 }
---
+++ src/component/Sales/chartConfigs.js
@@ -0,0 +1,150 @@
+const chartConfigs = [
+    {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'Dataset 1',
+            data: [12, 19, 3, 5, 2, 3, 9],
+            fill: false,
+            borderColor: 'rgb(255, 99, 132)',
+            tension: 0.1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    },
+    {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: 'Dataset 2',
+            data: [12, 19, 8, 17, 11, 15],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    },
+    {
+      type: 'pie',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: 'Dataset 2',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false
+          }
+        },
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    },
+    {
+      type: 'bubble',
+      data: {
+        datasets: [
+          {
+            label: 'Bubble Chart Dataset',
+            data: [
+              {
+                x: 20,
+                y: 30,
+                r: 15
+              },
+              {
+                x: 40,
+                y: 10,
+                r: 12
+              },
+              {
+                x: 60,
+                y: 40,
+                r: 10
+              },
+              {
+                x: 35,
+                y: 35,
+                r: 10
+              }
+            ],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+          }
+        ]
+      },
+      options: {
+        scales: {
+          x: {
+            type: 'linear',
+            position: 'bottom',
+            title: {
+              display: true,
+              text: 'X Axis Title'
+            }
+          },
+          y: {
+            type: 'linear',
+            position: 'left',
+            title: {
+              display: true,
+              text: 'Y Axis Title'
+            }
+          }
+        }
+      }
+    }
+  ];
+
+  export default chartConfigs
