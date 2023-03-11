2639cb4a39c6de794afd47be3c233c71a954e514
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-03-11 23:48:49 +0600

    working on the calendar page

--- src/pages/Calender/Calendar.js
+++ src/pages/Calender/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import NavBar from '../../component/navBar/NavBar'
 import Sidebar from '../../component/sidebar/Sidebar'
 import NavExpandHook from '../../hooks/NavExpandHook'
@@ -6,27 +6,54 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import ReactDatePicker from 'react-datepicker'
 import './calendar.css'
+import CalenderModal from './CalenderModal'
+
+
+const events = [
+  {
+    title: 'The Title of the day is the day',
+    start: new Date(new Date().setDate(new Date().getDate() - 2)),
+    end: new Date(new Date().setDate(new Date().getDate() + 2)),
+  },
+  {
+    title: 'Important Meeting',
+    start: new Date(new Date().setDate(new Date().getDate() - 7)),
+    end: new Date(new Date().setDate(new Date().getDate() - 7)),
+  },
+  {
+    title: 'Vacation',
+    start: new Date(new Date().setDate(new Date().getDate() - 6)),
+    end: new Date(new Date().setDate(new Date().getDate() - 3)),
+  },
+  {
+    title: 'Conference',
+    start: new Date(new Date().setDate(new Date().getDate() - 0)),
+    end: new Date(new Date().setDate(new Date().getDate() - 0)),
+  }
+  
+]
 
 const Calendar = () => {
     const {sidebarSize} = useContext(NavExpandHook)
     const navbarSize = `calc(100vw - ${sidebarSize}px)`
 
-    const events = [
-      {
-        title: 'The Title of the day is the day',
-        start: new Date(new Date().setDate(new Date().getDate() - 2)),
-        start: new Date(new Date().setDate(new Date().getDate() + 2)),
-      }
-    ]
-
     //calendar event data pass tutorial
     //https://www.youtube.com/watch?v=lyRP_D0qCfk
 
-    
+    const [newEvent, setNewEvent] = useState({title: '', start: '', end: ''})
+    const [allEvents, setAllEvents] = useState(events)
+    const [eventBox, setEventBox] = useState(false)
+
+    function handleAddEvent(){
+      setAllEvents([...allEvents, newEvent])
+    }
+
 
   return (
     <div style={{ display: 'flex', width: '100vw'}}>
+        {eventBox && <CalenderModal setEventBox={setEventBox} eventBox={eventBox} />}
         <Sidebar />
         <div style={{width: navbarSize}}>
             <NavBar />
@@ -34,20 +61,28 @@ const Calendar = () => {
                 <div>
                     <FullCalendar 
                        plugins = {[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+                       customButtons={{
+                        testButton : {
+                          text: 'Add new event',
+                          click: function () {
+                            setEventBox(!eventBox);
+                          },
+                          }
+                      }}
                        headerToolbar={{
-                         left: 'prev, next, today',
+                         left: 'prev, next, testButton',
                          center: 'title',
                          right: 'dayGridMonth, timeGridWeek, timeGridDay'
                        }}
                        allDaySlot={false}
-                       initialView="timeGridWeek"
+                       initialView="dayGridMonth"
                        slotDuration={"01:00:00"}
                        editable={true}
                        selectable={true}
                        selectMirror={true}
                        weekends={true}
                        nowIndicator={true}
-                       events={events}
+                       events={allEvents}  
                     />
                 </div>
             </div>
---
+++ src/pages/Calender/CalenderModal.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import style from './calenderModel.module.css'
+import { RxCross2 } from 'react-icons/rx'
+import ReactDatePicker from 'react-datepicker'
+
+const styleComponent = {  
+    position: 'absolute',
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: 'rgba(128, 128, 128, 0.7)',
+    display: 'flex',
+    justifyContent: 'center',
+    zIndex: 100
+}
+
+const styleMainDiv = {
+    backgroundColor: 'white',
+    height: '300px',
+    minWidth: '400px',
+    marginTop: '20px',
+    borderRadius: '5px',
+    padding: '30px'
+}
+
+
+const CalenderModal = ({setEventBox, eventBox}) => {
+      const [eventName, setEventName] = useState('')
+      const [eventStart, setEventStart] = useState('');
+      const [eventEnd, setEventEnd] = useState('');
+    
+        const handleSave = (e) =>{
+            e.preventDefault()
+            const eventData = {
+                eventName,
+                eventStart,
+                eventEnd
+            }
+            console.log(eventData)
+            setEventBox(!eventBox)
+        }
+  return (
+    <div style={styleComponent}>
+        <div style={styleMainDiv}>
+            <div className={style.header}>
+                <h3>Add Event</h3>
+                <RxCross2 className={style.close} onClick={()=> setEventBox(!eventBox)} />
+            </div>
+            <form className={style.body}>
+                <div className={style.formControl}>
+                    <p>Event Name</p>
+                    <input 
+                       type="text" 
+                       placeholder='Insert Event Name' 
+                       value={eventName} 
+                       onChange={(e)=> setEventName(e.target.value)} 
+                    />
+                </div>
+                <div className={`${style.formControl} ${style.eventForm}`}>
+                    <div>
+                        <p>Event Start</p>
+                        <div className={style.datePicker}>
+                                <ReactDatePicker 
+                                    placeholderText='Insert Event Start' 
+                                    selected={eventStart}
+                                    style={{width: '100%', backgroundColor: 'white'}}
+                                    onChange={(date)=> setEventStart(date)}  
+                                    className={style.reactDatePicker}                                 
+                                />
+                        </div>
+                    </div>
+                    <div>
+                        <p>Event End</p>
+                        <ReactDatePicker 
+                            placeholderText='Insert Event End' 
+                            selected={eventEnd}
+                            style={{width: '100%', backgroundColor: 'white'}}
+                            onChange={(date)=> setEventEnd(date)}
+                        />
+                    </div>
+                </div>
+                <div className={`${style.formButton}`}>
+                    <button className={style.closeBtn} onClick={()=> setEventBox(!eventBox)}>Close</button>
+                    <button className={style.saveBtn} onClick={handleSave}>Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default CalenderModal
